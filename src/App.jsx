62e2f05a288252e9c29ce16816454fa5f38bb266
lucianoideh <vazquezluciano5@gmail.com>
2024-03-19 13:07:27 -0300

    first commit & header nav

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+    <h1>Cv-application</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import "../styles/header.css"
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <span>Cv-Application</span>
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Tool</a></li>
+                <li><a href="#">Footer</a></li>
+            </ul>
+            <span></span>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
