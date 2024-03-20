08f3300f72707423a55f0232909423d8ebf46e50
lucianoideh <vazquezluciano5@gmail.com>
2024-03-20 16:15:49 -0300

    replaced form comp for jsx code and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import Form from "./components/Form"
 import Header from "./components/Header"
 import { useState } from "react"
-import Results from "./components/Results"
 function App() {
 
   const [test,setTest] = useState("")
-  function handleInputChange(e){
-    const target = e.target
-    const value = e.value
-    const name =  target.name
 
-    setTest({
-      [name]:value
-    })
-  }
 
   return (
     <>
     <Header></Header>
     <main>
-      {/* <Form test={test} setTest={setTest} handleOnInputChange={handleInputChange}></Form>
-      <Results test={test}></Results> */}
+      <form>
+        <label>Name: 
+          <input type="text" 
+          name="name"
+          value={test}
+          onChange={e => setTest(e.target.value)}/>
+        </label>
+        <br />
+      </form>
+      <div className="results">
+        {test}
+      </div>
     </main>
     </>
   )
