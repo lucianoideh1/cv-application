4198b6c1c9746bacb882fdc60bb0dc95c16bc628
lucianoideh <vazquezluciano5@gmail.com>
2024-03-27 17:42:28 -0300

    created styles for footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,8 @@
 import "../styles/footer.css"
 const Footer = () => {
   return (
-    <div>
-        Info
-        Developed by @lucianoideh
-        <ul>
+    <div className="footer">
+        <ul className="social-media-links">
             <li><a href="#">twitter</a></li>
             <li><a href="#">github</a></li>
             <li><a href="#">linkedin</a></li>
