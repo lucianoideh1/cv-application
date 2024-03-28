e2d0bdfc4e652e76bb439fad1b2dce1e80952d70
lucianoideh <vazquezluciano5@gmail.com>
2024-03-28 15:45:31 -0300

    styled main, form and removed grid lines

--- src/App.jsx
+++ src/App.jsx
@@ -50,75 +50,58 @@ function App() {
           <legend>Personal info</legend>
               <legend>General info</legend>
               <label>Nombre: 
-                <br />
                 <input type="text" 
                 name="name"
                 value={objeto.name}
                 onChange={handleChange}/>
               </label>
-            <br />
             <label>Role Position: 
-                <br />
                 <input type="text" 
                 name="position"
                 value={objeto.position}
                 onChange={handleChange}/>
               </label>
-            <br />
               <label>Email: 
-              <br />
                 <input type="email" 
                 name="email"
                 value={objeto.email}
                 onChange={handleChange}/>
               </label>
-            <br />
               <label>Phone: 
-              <br />
                 <input type="number" 
                 name="phone"
                 value={objeto.phone}
                 onChange={handleChange}/>
               </label>
-            <br />
           </fieldset>
 
           <fieldset>
             <legend>Past experiences</legend>
             <label>Company name:
-            <br />
               <input type="text"
               name="company_name"
               value={objeto.company_name}
               onChange={handleChange}/> 
             </label>
-            <br />
             <label>Position title:
-            <br />
               <input type="text" 
               name="position_title"
               value={objeto.position_title}
               onChange={handleChange}/> 
             </label>
-            <br />
             <label>Responability:
-            <br />
               <input type="text"
                name="responsability"
                value={objeto.responsability}
                onChange={handleChange}/>
             </label>
-            <br />
             <label>Start:
-            <br />
               <input type="text" 
                name="work_start"
                value={objeto.work_start}
                onChange={handleChange}/>
             </label>
-            <br />
             <label>End:
-            <br />
               <input type="text" 
                name="work_end"
                value={objeto.work_end}
@@ -129,31 +112,24 @@ function App() {
           <fieldset>
             <legend>Studies</legend>
             <label>School name:
-            <br />
               <input type="text" 
                name="school_name"
                value={objeto.school_name}
                onChange={handleChange}/>
             </label>
-            <br />
             <label>Title of study:
-            <br />
               <input type="text" 
                name="study_title"
                value={objeto.study_title}
                onChange={handleChange}/>
             </label>
-            <br />
             <label>Start:
-            <br />
               <input type="text" 
                name="start"
                value={objeto.start}
                onChange={handleChange}/>
             </label>
-            <br />
             <label>End:
-            <br />
               <input type="text"
                name="end"
                value={objeto.end}
@@ -161,14 +137,10 @@ function App() {
             </label>
           </fieldset>
           <input type="submit" value="Submit" />
-
           </form>
-    {/* <Results2 objeto={objeto}></Results2> */}
     {completedForm? <Results2 objeto={objeto}></Results2> : null}
 
     </main>
-    {/* {completedForm? <Result objeto={objeto}></Result> : null} */}
-
     <About id="about"></About>
     <Footer id="footer"></Footer>
 
