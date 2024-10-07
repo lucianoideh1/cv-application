713b1465d572606e9c9552234f8fcbfa2a4f2e51
Luciano Vazquez <vazquezluciano5@gmail.com>
2024-10-07 15:55:00 -0300

    add props to studies components

--- src/components/Studies.jsx
+++ src/components/Studies.jsx
@@ -1,7 +1,21 @@
-const Studies = () => {
+import PropTypes from  'prop-types'
+
+const Studies = ({title_study, school_name, start, end}) => {
   return (
-    <div>Studies</div>
+    <div className="studies">
+    <p className="section-title">Certificates</p>
+    <p>{title_study}</p>
+    <p>{school_name}</p>
+    <p>{start} - {end}</p>
+</div>
   )
+}
+
+Studies.propTypes = {
+    title_study: PropTypes.string,
+    school_name: PropTypes.string,
+    start: PropTypes.string,
+    end: PropTypes.string
 }
 
 export default Studies
