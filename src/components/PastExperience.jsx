e78dce9557ea538249461cff4eb949c1fd7e4c0a
Luciano Vazquez <vazquezluciano5@gmail.com>
2024-10-07 15:53:42 -0300

    add props to past experience component

--- src/components/PastExperience.jsx
+++ src/components/PastExperience.jsx
@@ -1,7 +1,24 @@
-const PastExperience = () => {
+import PropTypes from  'prop-types'
+
+const PastExperience = ({position_title, company_name, work_start, work_end, responsability}) => {
+
   return (
-    <div>PastExperience</div>
+    <div className="work-experience">
+    <p className="section-title">Experience</p>
+    <p>{position_title}</p>
+    <p>{company_name}</p>
+    <p>{work_start} - {work_end}</p>
+    <p>{responsability}</p>
+</div>
   )
+}
+
+PastExperience.propTypes = {
+    position_title: PropTypes.string,
+    company_name: PropTypes.string,
+    work_start: PropTypes.string,
+    work_end: PropTypes.string,
+    responsability: PropTypes.string,
 }
 
 export default PastExperience
